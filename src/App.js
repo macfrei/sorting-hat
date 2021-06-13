3135d0d4f95b25cba5495497e997f64cd9dc1562
Marie-Carleen Frei <lene@7freiheiten.de>
2021-06-13 17:36:47 +0200

    Update App.js with Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import questions from './assets/questions.json'
+import Form from './components/Form'
+
 function App() {
-  return <div>Sorting Hat</div>
+  return <Form questions={questions} onSaveHouse={saveHouse} />
+
+  function saveHouse(data) {
+    console.log(data)
+  }
 }
 
 export default App
