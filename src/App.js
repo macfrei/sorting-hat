7e6ed783960cb0107949154390652655d6bb9a43
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2023-01-03 14:53:31 +0900

    Add functionality to find house with most corresponding answers

--- src/App.js
+++ src/App.js
@@ -10,7 +10,41 @@ function App() {
   )
 
   function saveHouse(data) {
-    console.log(data)
+    const houseQuizAnswers = Object.entries(data)
+    const listOfHousesAccordingToAnswers = houseQuizAnswers.flatMap(
+      answerArray => {
+        const associatedQuestion = questions.find(
+          el => el.question === answerArray[0]
+        )
+        const possibleAnswers = Object.entries(associatedQuestion.answers) // [[house, answer], ...]
+
+        const houses = possibleAnswers
+          .filter(answer => {
+            // Because answer could be an array of multiple possibilities
+            if (answer.filter(e => e.includes(answerArray[1])).length > 0) {
+              return answer
+            }
+
+            return answer.includes(answerArray[1])
+          })
+          .map(possibleAnswer => possibleAnswer[0]) // Corresponding house
+
+        return houses
+      }
+    )
+
+    const house = Object.entries(
+      listOfHousesAccordingToAnswers.reduce((acc, cur) => {
+        if (cur in acc) {
+          acc[cur]++
+        } else {
+          acc[cur] = 1
+        }
+        return acc
+      }, {})
+    ).sort((a, b) => b[1] - a[1])[0][0]
+
+    console.log(house)
   }
 }
 
